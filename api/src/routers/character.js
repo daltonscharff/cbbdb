fffdf83061d332236feaab9942453fd2aeb6e0ba
Dalton Scharff <dalton.scharff@gmail.com>
2019-09-28 09:35:08 -0500

    adds character routes

--- api/src/routers/character.js
+++ api/src/routers/character.js
@@ -5,8 +5,70 @@ const Guest = require('../models/guest');
 
 const router = new express.Router();
 
-router.get('/characters', (req, res) => {
-    res.send('hello from /characters');
-});
+router.route('/characters')
+    .get(async (req, res) => {
+        try {
+            const characters = await Character.find({});
+            res.send(characters);
+        } catch (e) {
+            res.status(500).send(e);
+            console.error({ error: e, method: req.method, url: req.url });
+        }
+    })
+    .post(async (req, res) => {
+        const character = new Character(req.body);
+
+        try {
+            await character.save();
+            res.status(201).send(character);
+        } catch (e) {
+            res.status(400).send(e);
+            console.error({ error: e, method: req.method, url: req.url });
+        }
+    });
+
+router.route('/characters/:id')
+    .get(async (req, res) => {
+        try {
+            const character = await Character.findOne({ _id: req.params.id });
+
+            if (!character) return res.status(404).send();
+
+            res.send(character);
+        } catch (e) {
+            res.status(500).send(e);
+            console.error({ error: e, method: req.method, url: req.url });
+        }
+    })
+    .patch(async (req, res) => {
+        const updates = Object.keys(req.body);
+
+        try {
+            const character = await Character.findOne({ _id: req.params.id });
+
+            if (!character) return res.status(404).send();
+
+            updates.forEach((update) => character[update] = req.body[update]);
+
+            await character.save();
+            res.send(character);
+        } catch (e) {
+            res.status(500).send(e);
+            console.error({ error: e, method: req.method, url: req.url });
+        }
+    })
+    .delete(async (req, res) => {
+        try {
+            const character = await Character.findOneAndDelete({ _id: req.params.id });
+    
+            if (!character) return res.status(404).send();
+    
+            res.send(character);
+        } catch (e) {
+            res.status(500).send(e);
+            console.error({ error: e, method: req.method, url: req.url });
+        }
+    });
+
 
 module.exports = router;
