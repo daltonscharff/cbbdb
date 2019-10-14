0b9196ccaa1e3b4dc5251a57e303a68eddeea273
Dalton Scharff <dalton.scharff@gmail.com>
2019-10-13 19:43:19 -0500

    adds testing files (but doesn't run yet)

---
+++ api/tests/fixtures/db.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose');
+const Character = require('../../src/models/character');
+const Episode = require('../../src/models/episode');
+const Guest = require('../../src/models/guest');
