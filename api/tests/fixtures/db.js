aa8aee384244abc51f2a0b427c39b0f1741bc9f1
Dalton Scharff <dalton.scharff@gmail.com>
2020-08-30 18:49:12 -0500

    removes all files but readme -- restaring from scratch

--- api/tests/fixtures/db.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose');
-const Character = require('../../src/models/character');
-const Episode = require('../../src/models/episode');
-const Guest = require('../../src/models/guest');
