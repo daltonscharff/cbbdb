04512eec2fe7581138231cd96ffbc1e365a42d3f
Dalton Scharff <dalton.scharff@gmail.com>
2020-08-30 18:43:38 -0500

    adds UI work

---
+++ gui/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EpisodeList from './EpisodeList';
+
+function App() {
+    return (
+        <div>
+            <EpisodeList />
+        </div>
+    );
+}
+
+export default App;
