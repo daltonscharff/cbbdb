29a805d02412cd80bd1a3e3bfece5bc93278111b
Dalton Scharff <dalton.scharff@gmail.com>
2020-08-30 20:18:26 -0500

    modifies get request to return json

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ const port: number = parseInt(process.env.PORT || "8000", 10);
 const host: string = process.env.HOST || "localhost";
 
 app.get('/', (req: express.Request, res: express.Response): void => {
-    res.send("Hello World");
+    res.send({ response: "OK" });
 });
 
 app.listen(port, () => console.log(`Server started on http://${host}:${port}`))
