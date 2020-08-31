5980c70f113d09b1be15d3e303c0b73ddf47bd52
Dalton Scharff <dalton.scharff@gmail.com>
2020-08-30 19:37:05 -0500

    creates a barebones express application with typescript

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app: express.Application = express();
+const port: number = parseInt(process.env.PORT || "8000", 10);
+const host: string = process.env.HOST || "localhost";
+
+app.get('/', (req: express.Request, res: express.Response): void => {
+    res.send("Hello World");
+});
+
+app.listen(port, () => console.log(`Server started on http://${host}:${port}`))
