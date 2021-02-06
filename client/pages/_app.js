9c2d420148a7926c13f5c32536ab8b7d43317eac
Dalton Scharff <dalton.scharff@gmail.com>
2021-02-05 22:13:38 -0600

    crossorigin to crossOrigin for react

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -9,7 +9,7 @@ function CBBDb({ Component, pageProps }) {
       <script
         src="https://code.jquery.com/jquery-3.1.1.min.js"
         integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-        crossorigin="anonymous"></script>
+        crossOrigin="anonymous"></script>
       <script src="semantic/dist/semantic.min.js"></script>
     </Head>
     <Component {...pageProps} />
