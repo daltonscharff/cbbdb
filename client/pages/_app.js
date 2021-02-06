e858dea0d55f84ecd8a0ac74c13ffbe3216c8e11
Dalton Scharff <dalton.scharff@gmail.com>
2021-02-06 13:38:36 -0600

    adds tailwindscss and removes semantic ui

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,19 +1,13 @@
-import '../styles/globals.css'
+import "tailwindcss/tailwind.css"
 import Head from 'next/head'
 
 function CBBDb({ Component, pageProps }) {
-  return <>
+  return <div>
     <Head>
       <link rel="icon" href="/favicon.ico" />
-      <link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css" />
-      <script
-        src="https://code.jquery.com/jquery-3.1.1.min.js"
-        integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-        crossOrigin="anonymous"></script>
-      <script src="semantic/dist/semantic.min.js"></script>
     </Head>
     <Component {...pageProps} />
-  </>
+  </div >
 }
 
 export default CBBDb
--- client/pages/index.js
+++ client/pages/index.js
@@ -3,36 +3,49 @@ import { cc } from "../services/contentful"
 
 export default function Episodes({ episodes, ...props }) {
   return (
-    <div>
+    <div className="">
       {episodes.map(episode => (
-        <div key={episode.id} id={episode.id}>
-          <p>ID: {episode.id}</p>
-          <p>Title: {episode.title}</p>
-          <p>Characters:
-          <ul>
-              {episode.characters.map(character => (
-                <Link key={character.id} href={`/characters/${character.id}`}>
-                  <li>
-                    {character.name || character.id}
-                  </li>
-                </Link>
-              ))}
-            </ul>
-          </p>
-          <p>Guests:
-          <ul>
-              {episode.guests.map(guest => (
-                <Link key={guest.id} href={`/guests/${guest.id}`}>
-                  <li>
-                    {guest.name || guest.id}
-                  </li>
-                </Link>
-              ))}
-            </ul>
-          </p>
+        <div
+          className=""
+          key={episode.id}
+          id={episode.id}
+        >
+          <div className="">
+
+            <div className="">{episode.title}</div>
+            <div className="text-gray-500 text-md">Ep. 111</div>
+            <div className="">
+              <div className="py-2">Description here.</div>
+              <div className="flex">
+                <div className="">
+                  <div className="">Characters</div>
+                  {episode.characters.map(character => (
+                    <Link key={character.id} href={`/characters/${character.id}`}>
+                      <a className="">
+                        {character.name}
+                        <br />
+                      </a>
+                    </Link>
+                  ))}
+                </div>
+                <div className="">
+                  <div className="">Guests</div>
+                  {episode.guests.map(guest => (
+                    <Link key={guest.id} href={`/guests/${guest.id}`}>
+                      <a className="">
+                        {guest.name}
+                        <br />
+                      </a>
+                    </Link>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      ))}
-    </div>
+      ))
+      }
+    </div >
   )
 }
 
