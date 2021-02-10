3fc23220ed40273cdfb8618452b2bae317da4a81
Dalton Scharff <dalton.scharff@gmail.com>
2021-02-09 19:29:55 -0600

    removes tailwind and adds both semantic and tailwind cdns

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,11 +1,11 @@
-import "tailwindcss/tailwind.css"
-import 'semantic-ui-css/semantic.min.css'
 import Head from 'next/head'
 
 function CBBDb({ Component, pageProps }) {
   return <div>
     <Head>
       <link rel="icon" href="/favicon.ico" />
+      <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet"></link>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"></link>
     </Head>
     <Component {...pageProps} />
   </div >
