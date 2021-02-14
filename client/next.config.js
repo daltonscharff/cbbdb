39fe62b64562a70a6781063b246839c2553230fc
Dalton Scharff <dalton.scharff@gmail.com>
2021-02-14 12:19:29 -0600

    removes i18n due to export failure

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,3 @@
 module.exports = {
-  basePath: process.env.PROJECT_BASEPATH || "",
-  i18n: {
-    locales: ['en-US'],
-    defaultLocale: 'en-US'
-  }
+  basePath: process.env.PROJECT_BASEPATH || ""
 }
